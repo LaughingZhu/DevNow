5809c3dc6e71574fd3835c27df04ccae6ae51be9
LaughingZhu <474268433@qq.com>
2024-06-27 23:30:05 +0800

    feat(*): config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+import { defineConfig, squooshImageService } from 'astro/config';
+import rehypePluginImageNativeLazyLoading from 'rehype-plugin-image-native-lazy-loading';
+import { remarkReadingTime } from './src/utils/all';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://devnow.com',
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    rehypePlugins: [rehypePluginImageNativeLazyLoading],
+    extendDefaultPlugins: true
+  },
+  integrations: [icon()],
+  image: {
+    service: squooshImageService()
+  },
+  integrations: [tailwind(), mdx(), sitemap()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.neutral
+      },
+      fontFamily: {
+        sans: ['InterVariable', 'Inter', ...defaultTheme.fontFamily.sans]
+      }
+      // aspectRatio: {
+      //   "4/3": "4 / 3",
+      //   "3/2": "3 / 2",
+      //   "2/3": "2 / 3",
+      //   "9/16": "9 / 16",
+      // },
+    }
+  },
+  plugins: [
+    // require("@tailwindcss/line-clamp"),
+    require('@tailwindcss/typography')
+  ]
+};
