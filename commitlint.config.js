63becc930b44023dc5acf41bd29c47bfc78dc6ec
zhuxiaochen <zhuxiaochen@mediatrack.cn>
2024-07-02 21:38:43 +0800

    feat: pre-commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ export default {
     'type-enum': [2, 'always', ['upd', 'feat', 'fix', 'docs', 'revert']],
     'type-case': [0],
     'subject-full-stop': [0, 'never'],
-    'scope-empty': [2, 'never'],
+    // 'scope-empty': [2, 'never'],
     'subject-case': [0, 'never'],
     'header-max-length': [0, 'always', 72]
   }
