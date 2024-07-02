2c4da3f224aa78dc22678c314526bd0c8c6b865d
zhuxiaochen <zhuxiaochen@mediatrack.cn>
2024-07-02 19:48:01 +0800

    fixx(*): lint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert']
-    ],
+    'type-enum': [2, 'always', ['upd', 'feat', 'fix', 'docs', 'style', 'revert']],
     'type-case': [0],
     'subject-full-stop': [0, 'never'],
     'scope-empty': [2, 'never'],
