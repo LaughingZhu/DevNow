9e50e2c0a9e060e9aa371f93f5c4151c195435a7
LaughingZhu <474268433@qq.com>
2024-09-03 15:40:06 +0800

    feat(*): 搜索组件使用 shadcn/command 替换 Algolia

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,18 +1,71 @@
+import config from '@/config';
 import '@common/docsearch.css';
-import config from '@config/index';
-import { DocSearch } from '@docsearch/react';
-const apiKey = import.meta.env.PUBLIC_SEARCH_API_KEY;
-const apiId = import.meta.env.PUBLIC_SEARCH_APP_ID;
+import { getAllCategories } from '@utils/content';
+import { Fragment, useEffect, useState } from 'react';
+import { Button } from './shadcn/button';
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator
+} from './shadcn/command';
+const category = await getAllCategories();
+
 function Search() {
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
   return (
-    <div className='mr-4 hidden lg:block'>
+    <div className='mr-4 hidden items-center lg:flex'>
+      <Button
+        onClick={() => setOpen((open) => !open)}
+        size='sm'
+        className='h-[32px] bg-muted/50 px-2 py-0 text-sm text-muted-foreground shadow-none hover:bg-accent focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring'
+      >
+        Search
+        <kbd className='pointer-events-none ml-1 inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'>
+          <span className='text-xs'>⌘</span>K
+        </kbd>
+      </Button>
+
       {config.search && (
-        <DocSearch
-          appId={apiId}
-          apiKey={apiKey}
-          indexName='devnow-laughingzhu'
-          maxResultsPerGroup={5}
-        />
+        <CommandDialog open={open} onOpenChange={setOpen}>
+          <CommandInput placeholder='Type a command or search...' />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            {category.map((item, index) => (
+              <Fragment key={index}>
+                <CommandGroup heading={item.label}>
+                  {item.children.map((child, cIndex) => (
+                    <CommandItem
+                      onSelect={() => {
+                        window.open(`${location.origin}/posts/${child.id}`, '_blank');
+                      }}
+                      key={cIndex}
+                      className='cursor-pointer'
+                    >
+                      <span>{child.label}</span>
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+                {index === category.length - 1 && <CommandSeparator />}
+              </Fragment>
+            ))}
+          </CommandList>
+        </CommandDialog>
       )}
     </div>
   );
