fc0820c8c76541b46fd56f59f4daee135b93648c
LaughingZhu <474268433@qq.com>
2024-10-06 15:58:27 +0800

    feat(*): search with lunr

---
+++ src/pages/search-docs.json.ts
@@ -0,0 +1,26 @@
+/*
+ * @Date: 2024-10-05 20:28:37
+ * @LastEditors: LaughingZhu 474268433@qq.com
+ * @LastEditTime: 2024-10-06 14:25:34
+ * @Description:
+ */
+import { latestPosts } from '@/utils/content';
+
+let documents = latestPosts.map((post) => {
+  return {
+    slug: post.slug,
+    title: post.data.title,
+    description: post.data.desc,
+    content: post.body,
+    category: post.data.category
+  };
+});
+
+export async function GET() {
+  return new Response(JSON.stringify(documents), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
---
+++ src/pages/search-index.json.js
@@ -0,0 +1,52 @@
+/*
+ * @Date: 2024-10-05 20:28:37
+ * @LastEditors: LaughingZhu 474268433@qq.com
+ * @LastEditTime: 2024-10-06 14:25:03
+ * @Description:
+ */
+import { latestPosts } from '@/utils/content';
+import lunr from 'lunr';
+import MarkdownIt from 'markdown-it';
+const stemmerSupport = await import('lunr-languages/lunr.stemmer.support.js');
+const zhPlugin = await import('lunr-languages/lunr.zh.js');
+// 初始化 stemmer 支持
+stemmerSupport.default(lunr);
+// 初始化中文插件
+zhPlugin.default(lunr);
+const md = new MarkdownIt();
+
+let documents = latestPosts.map((post) => {
+  return {
+    slug: post.slug,
+    title: post.data.title,
+    description: post.data.desc,
+    content: md.render(post.body)
+  };
+});
+export const LunrIdx = lunr(function () {
+  this.use(lunr.zh);
+  this.ref('slug');
+  this.field('title');
+  this.field('description');
+  this.field('content');
+  // This is required to provide the position of terms in
+  // in the index. Currently position data is opt-in due
+  // to the increase in index size required to store all
+  // the positions. This is currently not well documented
+  // and a better interface may be required to expose this
+  // to consumers.
+  // this.metadataWhitelist = ['position'];
+
+  documents.forEach((doc) => {
+    this.add(doc);
+  }, this);
+});
+
+export async function GET() {
+  return new Response(JSON.stringify(LunrIdx), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
