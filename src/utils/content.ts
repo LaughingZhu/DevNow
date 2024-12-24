2ca80f118a33cf9ae8b3a101d0e23d5d7be9b28b
zhuxiaochen <474268433@qq.com>
2024-12-24 18:16:57 +0800

    feat: DevNow x Notion

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -1,28 +1,27 @@
 import { categories } from '@data/category';
 import { getCollection } from 'astro:content';
-import { richTextToPlainText } from 'notion-astro-loader';
-import { formatDate } from './utils';
 
-export const getNotionDocs = (await getCollection('notion')).map((item) => {
-  return {
-    id: item.id,
-    body: item.rendered?.html ?? '',
-    data: {
-      title: richTextToPlainText(item.data.properties.title.rich_text),
-      desc: richTextToPlainText(item.data.properties.desc.rich_text),
-      category: item.data.properties.category.select?.name || '',
-      author: richTextToPlainText(item.data.properties.author.rich_text),
-      tags: item.data.properties.tags.rich_text[0].plain_text.split(','),
-      image: item.data.properties.image.url,
-      publishDate: formatDate(item.data.properties.publishDate.created_time),
-      pin: false
-    },
-    rendered: item.rendered,
-    filePath: item.id,
-    collection: item.collection,
-    name: richTextToPlainText(item.data.properties.title.rich_text)
-  };
-});
+// 如果需要接入 Notion 数据源，需要将下面的注释去掉
+// export const getNotionDocs = (await getCollection('notion')).map((item) => {
+//   return {
+//     id: item.id,
+//     body: item.rendered?.html ?? '',
+//     data: {
+//       title: richTextToPlainText(item.data.properties.title.rich_text),
+//       desc: richTextToPlainText(item.data.properties.desc.rich_text),
+//       category: item.data.properties.category.select?.name || '',
+//       author: richTextToPlainText(item.data.properties.author.rich_text),
+//       tags: item.data.properties.tags.rich_text[0].plain_text.split(','),
+//       image: item.data.properties.image.url,
+//       publishDate: formatDate(item.data.properties.publishDate.created_time),
+//       pin: false
+//     },
+//     rendered: item.rendered,
+//     filePath: item.id,
+//     collection: item.collection,
+//     name: richTextToPlainText(item.data.properties.title.rich_text)
+//   };
+// });
 
 // Only return posts without `draft: true` in the frontmatter
 export const latestPosts = [
