990a349a1cfc444a0ae919a74808c2cd263f9710
sigazen <shigazenanna@gmail.com>
2025-01-02 12:09:27 +0800

    Update category.ts
    Signed-off-by: sigazen <shigazenanna@gmail.com>

--- src/data/category.ts
+++ src/data/category.ts
@@ -2,28 +2,27 @@ import type { Category } from '@type/category';
 
 export const categories: Category[] = [
   {
-    title: 'Technology',
-    slug: 'technology',
-    color: 'blue',
-    description:
-      'Keep up with the latest tech trends and learn about the latest innovations in software development, hardware design, cybersecurity, and more.'
+    title: '发电厂',
+    slug: 'power-station',
+    color: '#ff4d4f',  // 活力红色，表示热情澎湃
+    description: '就是siga的发癫。'
   },
   {
-    title: 'Book',
-    slug: 'book',
-    color: 'green',
-    description: 'Something about Books'
+    title: '教程分享',
+    slug: 'tutorials',
+    color: '#52c41a',  // 生机绿色，代表成长与分享
+    description: '原创或搬运的各类实用教程。'
   },
   {
-    title: 'Blog',
-    slug: 'blog',
-    color: 'purple',
-    description: 'Something about Blog posts'
+    title: '学习笔记',
+    slug: 'study-notes',
+    color: '#1890ff',  // 知性蓝色，象征专注与智慧
+    description: '记录个人学习历程、心得体会、知识积累。'
   },
   {
-    title: 'DevNow',
-    slug: 'devnow',
-    color: 'orange',
-    description: 'Something about DevNow platform.'
+    title: '前言快讯',
+    slug: 'news-flash',
+    color: '#722ed1',  // 高贵紫色，表示洞察与视野
+    description: '追踪热点事件，分享时事动态，带来及时的资讯更新与个人见解。'
   }
 ];
